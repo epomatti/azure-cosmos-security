61ffff4c067f79cb1bbb05791e1e0f334b526f95
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-02-27 21:31:56 -0300

    cosmos container

--- modules/cosmos/main.tf
+++ modules/cosmos/main.tf
@@ -39,3 +39,32 @@ resource "azurerm_cosmosdb_sql_database" "db001" {
   resource_group_name = var.resource_group_name
   account_name        = azurerm_cosmosdb_account.db.name
 }
+
+resource "azurerm_cosmosdb_sql_container" "c001" {
+  name                  = "example-container"
+  resource_group_name   = var.resource_group_name
+  account_name          = azurerm_cosmosdb_account.db.name
+  database_name         = azurerm_cosmosdb_sql_database.db001.name
+  partition_key_path    = "/definition/id"
+  partition_key_version = 1
+
+  indexing_policy {
+    indexing_mode = "consistent"
+
+    included_path {
+      path = "/*"
+    }
+
+    included_path {
+      path = "/included/?"
+    }
+
+    excluded_path {
+      path = "/excluded/?"
+    }
+  }
+
+  unique_key {
+    paths = ["/definition/idlong", "/definition/idshort"]
+  }
+}
